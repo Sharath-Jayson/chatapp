5be797699e70ca270ff70829de34b2b75791f1a8
Sharath-Jayson <jaisarat@gmail.com>
2019-09-24 16:39:53 +0530

    Hello World

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello World");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => res.send('HEllo worldl'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
