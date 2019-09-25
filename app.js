30a40b4e8666c38a989b567f40a95659448692cb
Sharath-Jayson <jaisarat@gmail.com>
2019-09-25 17:04:29 +0530

    Added Bootstrap framework. Completing Chapter 1

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
+
+
+app.get('/', (req, res) => res.send('Hello World Welcome'));
+app.get('/home', (req, res) => res.send('Hello World Welcome to Home, Saavy!!'));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
